3a16206363b4833ed6bca32056f73411a84163d8
Cyril "Kyriog" Glapa <cyril.glapa@keuse.fr>
2014-02-21 02:52:39 +0100

    First version

---
+++ README.md
@@ -0,0 +1,12 @@
+mkd-pwnd
+========
+
+Chrome extension to pwn milkdome anti-adblock
+
+How to install?
+---------------
+
+This is the source code of the extension, you can check it to be sure that it doesn't do any evil thing!
+But if you want to use it, it won't be useful to you.
+The easiest way to install the extension is to follow our tutorial by checking the wiki:
+https://github.com/kyriog/ecl-pwnd/wiki/How-to-install-the-extension%3F
---
+++ mkd-pwn.js
@@ -0,0 +1,15 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * the COPYING file for more details. */
+
+var observer = new MutationObserver(function() {
+	pwnJs = document.createElement("script");
+	pwnJs.innerHTML  = "const __adBlock = false;";
+
+	document.body.appendChild(pwnJs);
+	observer.disconnect();
+});
+var config = { subtree: true, attributes: true }
+observer.observe(document, config);
